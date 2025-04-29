f2a3c7629879e755c2f8a6146aa2bd44e1ab0e15
rajgoutam123 <codingninja194@gmail.com>
2025-04-29 11:56:23 +0530

    Create JwtUtil.java

---
+++ src/main/java/com/parking/auth/Security/JwtUtil.java
@@ -0,0 +1,21 @@
+package com.parking.auth.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private static final String SECRET_KEY = "mysecretkey";
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours validity
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+}
