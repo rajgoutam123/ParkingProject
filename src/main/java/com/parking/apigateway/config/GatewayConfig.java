fad8a4a682f497fc60570329ded4ea4e0b98f574
rajgoutam123 <codingninja194@gmail.com>
2025-04-29 12:15:07 +0530

    Update GatewayConfig.java

--- src/main/java/com/parking/apigateway/config/GatewayConfig.java
+++ src/main/java/com/parking/apigateway/config/GatewayConfig.java
@@ -1,1 +1,34 @@
+package com.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.gateway.filter.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Configuration
+public class GatewayConfig {
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+            .route("auth_service", r -> r.path("/api/auth/**")
+                    .filters(f -> f.filter(jwtAuthenticationFilter.apply(new JwtAuthenticationFilter.Config()).setAuthenticationRequired(false)))
+                    .uri("lb://AUTH-SERVICE"))
+            .route("parking_service", r -> r.path("/api/parking/**")
+                    .filters(f -> f.filter(jwtAuthenticationFilter.apply(new JwtAuthenticationFilter.Config())))
+                    .uri("lb://PARKING-SERVICE"))
+            .route("booking_service", r -> r.path("/api/booking/**")
+                    .filters(f -> f.filter(jwtAuthenticationFilter.apply(new JwtAuthenticationFilter.Config())))
+                    .uri("lb://BOOKING-SERVICE"))
+            .route("payment_service", r -> r.path("/api/payment/**")
+                    .filters(f -> f.filter(jwtAuthenticationFilter.apply(new JwtAuthenticationFilter.Config())))
+                    .uri("lb://PAYMENT-SERVICE"))
+            .build();
+    }
+}
 
