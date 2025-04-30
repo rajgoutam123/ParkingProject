689ff675c9b2e4e4c7097b83ceb249bdd17d530b
rajgoutam123 <codingninja194@gmail.com>
2025-04-30 08:11:44 +0530

    Create User2.java

---
+++ src/main/java/com/parking/auth/entity/User2.java
@@ -0,0 +1,25 @@
+package com.parking.auth.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role; // USER, ADMIN, LOT_MANAGER
+}
